5a1c2c4412b329d19737593acadbf663996acfa3
dg-dhg <630909931@qq.com>
2020-05-31 03:14:04 +0800

    使用指定的配置文件，以及使用SPEL表达式

--- src/main/java/net/dhg/crm/modelling/Person.java
+++ src/main/java/net/dhg/crm/modelling/Person.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -19,15 +16,23 @@ import java.util.Map;
 @NoArgsConstructor
 @Component//即为组件的意思
 @ToString
+//@PropertySource("classpath:dengsir.yml")
 @ConfigurationProperties(prefix = "person")
 public class Person {
+//    @Value("${name}")
     private String name;
+//    @Value("${age}")
     private Integer age;
+//    @Value("${happy}")
     private Boolean happy;
+//    @Value("${birth}")
     private Date birth;
+//    @Value("${map}")
     private Map<String,Object> map;
+//    @Value("${list}")
     private List<Object> list;
     //大抵是通过反射机制加载配置文件，在初始化的时候为变量赋值，恐怕有一种处理机制使得配置的信息可以传递给特定的类
+//    @Value("${dog}")
     private Dog dog;
 
 
