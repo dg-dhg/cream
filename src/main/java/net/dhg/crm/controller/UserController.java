5ff8939263eeec6feb9db1f996cc2ce2586f4ad7
dg-dhg <630909931@qq.com>
2020-06-03 21:43:23 +0800

    测试登录跳转

--- src/main/java/net/dhg/crm/controller/UserController.java
+++ src/main/java/net/dhg/crm/controller/UserController.java
@@ -6,14 +6,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.thymeleaf.util.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+
     @RequestMapping("/index")
-    @ResponseBody
-    public String index(@RequestParam String username, @RequestParam String password, Model model){
-        return "ok";
+    public String index(@RequestParam String username, @RequestParam String password, Model model, HttpServletRequest request){
+        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+            model.addAttribute("msg","用户名或密码错误");
+            return "index";
+        }
+        else{
+            model.addAttribute("msg","用户名或密错误");
+            return "login";
+        }
+
     }
 }
